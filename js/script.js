ab7abf464155aa01c5561bcf4c5f7204d5857b92
CNakashiba <CNakashiba@gmail.com>
2017-08-29 18:55:25 -0700

    v20
    lines in the nav are visible when in that section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
+// $(document).ready(function(){
+
+// // scrolling from one part of the page to another
+// 	$('a').on('click', function (e){
+// 		e.preventDefault();
+// 		var target = $(this)[0]['hash']
+// 		var $target = $(target);
+
+// 		$('body').stop().animate({
+// 			'scrollTop': $target.offset().top
+// 		}, 900, 'swing', function(){
+// 			window.location.hash = target;
+// 		});
+// 	});
+// });
+
 $(document).ready(function(){
 
+		
+
 // scrolling from one part of the page to another
+// ===============================================
 	$('a').on('click', function (e){
 		e.preventDefault();
 		var target = $(this)[0]['hash']
@@ -8,8 +27,16 @@ $(document).ready(function(){
 
 		$('body').stop().animate({
 			'scrollTop': $target.offset().top
-		}, 900, 'swing', function(){
+		}, 
+		900, 'swing', function(){
 			window.location.hash = target;
-		});
+		}); 
+		// $('#headerLinks li').show()
+		
+		$(this).attr('class', 'active'); 			
 	});
 });
+
+
+
+
